6ba2262181872fa1199865f797da68a6bd759b3f
Mario <mario@donguto.com>
2026-07-08 00:11:36 -0500

    feat: asociar ids biométricos de ZKTeco a colaboradores y auditor en los semilleros

--- api/manage-users.js
+++ api/manage-users.js
@@ -88,23 +88,23 @@ export default async function handler(req, res) {
           { username: 'cnizamadg', password: 'dg.chri.N9633', name: 'Christian Nizama', role: 'Administrador', store: '28 de Julio Miraflores' },
           { username: 'arianadg', password: 'dg.aria.A9928', name: 'Ariana', role: 'Auditor', store: '28 de Julio Miraflores' },
           { username: 'ccuevadg', password: 'dg.chri.C9458', name: 'Christian Cueva', role: 'Administrador', store: 'Todas' },
-          { username: 'woviedodg', password: 'dg.wilf.O9580', name: 'Wilfredo Oviedo', role: 'Auditor', store: 'Todas' },
+          { username: 'woviedodg', password: 'dg.wilf.O9580', name: 'Wilfredo Oviedo', role: 'Auditor', store: 'Todas', biometric_id: '41670259' },
           { username: 'jortizdg', password: 'dg.juan.O9040', name: 'Juan Ortiz', role: 'Administrador', store: 'Todas' },
           { username: 'mquispedg', password: 'dg.mari.Q9008', name: 'Mario Quispe', role: 'Gerente', store: 'Todas' },
           { username: 'mquispetec', password: 'dg.mari.T8997', name: 'Mario Quispe (Técnico)', role: 'Técnico', store: 'Todas' },
           
           { username: 'avasquezdg', password: 'dg.alex.V38314', name: 'Alexander Vásquez Villalobos', role: 'Servicio', store: '28 de Julio Miraflores', email: 'Alexito1836@gmail.com', telefono: '992838314', biometric_id: '61096401' },
           { username: 'ddazadg', password: 'dg.daye.D65912', name: 'Dayerlin Carolina Daza Vargas', role: 'Barista', store: '28 de Julio Miraflores', email: 'dayerlincarolina.dv@gmail.com', telefono: '963365912' },
-          { username: 'mbravodg', password: 'dg.moni.B75773', name: 'Mónica Daniela Bravo Rodríguez', role: 'Servicio', store: '28 de Julio Miraflores', email: 'Monikbrav7@gmail.com', telefono: '908757732' },
+          { username: 'mbravodg', password: 'dg.moni.B75773', name: 'Mónica Daniela Bravo Rodríguez', role: 'Servicio', store: '28 de Julio Miraflores', email: 'Monikbrav7@gmail.com', telefono: '908757732', biometric_id: '06587622' },
           { username: 'aocampodg', password: 'dg.alex.O37809', name: 'Alexis Ocampo Rodríguez', role: 'Cocina', store: '28 de Julio Miraflores', email: 'Alexisjo@gmail.com', telefono: '945837809' },
-          { username: 'fsotodg', password: 'dg.fran.S04464', name: 'Franchesca Giovana Soto Chávez', role: 'Cocina', store: '28 de Julio Miraflores', email: 'fgschavez@gmail.com', telefono: '958004464' },
+          { username: 'fsotodg', password: 'dg.fran.S04464', name: 'Franchesca Giovana Soto Chávez', role: 'Cocina', store: '28 de Julio Miraflores', email: 'fgschavez@gmail.com', telefono: '958004464', biometric_id: '72306939' },
           { username: 'eegocheagadg', password: 'dg.emil.E54227', name: 'Emily Egocheaga Ormeño', role: 'Cocina', store: '28 de Julio Miraflores', email: 'egocheaga888@gmail.com', telefono: '904054227' },
           { username: 'psilvadg', password: 'dg.patr.S26393', name: 'Patrick Silva Chávez', role: 'Barista', store: '28 de Julio Miraflores', email: 'murciegus@gmail.com', telefono: '979526393' },
           { username: 'jaymadg', password: 'dg.jesu.A22582', name: 'Jesus Ayma Chaparro', role: 'Barista', store: '28 de Julio Miraflores', email: 'jesusaymachaparro@gmail.com', telefono: '912322582', biometric_id: '60979426' },
           { username: 'rlaurentedg', password: 'dg.ruth.L53898', name: 'Ruth Sarahi Laurente Olivera', role: 'Barista', store: '28 de Julio Miraflores', email: 'Sarahilaurente.7@gmail.com', telefono: '982953898' },
           { username: 'amosqueradg', password: 'dg.anto.M43801', name: 'Antonio Mosquera', role: 'Operaciones', store: 'Todas', biometric_id: '43801971' },
-          { username: 'cvidaldg', password: 'dg.ciro.V85721', name: 'Ciro Svith Vidal Ignacio', role: 'Cocina', store: '28 de Julio Miraflores' },
-          { username: 'aolivosdg', password: 'dg.aria.O72619', name: 'Ariana Olivos', role: 'Servicio', store: '28 de Julio Miraflores' }
+          { username: 'cvidaldg', password: 'dg.ciro.V85721', name: 'Ciro Svith Vidal Ignacio', role: 'Cocina', store: '28 de Julio Miraflores', biometric_id: '61268415' },
+          { username: 'aolivosdg', password: 'dg.aria.O72619', name: 'Ariana Olivos', role: 'Servicio', store: '28 de Julio Miraflores', biometric_id: '147242' }
         ];
 
         for (const userToSeed of usersToSeed) {
